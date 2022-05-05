bb3b56de334f661407bdb1824e4be8fa874e69b2
Evan Zuo <v_wangxiangbo01@baidu.com>
2022-05-05 21:45:57 +0800

    Revert "feat: time"
    This reverts commit 83cecccc05063c61b28aafddc7348287b055a4f1.

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2022-04-26 18:06:25
- * @LastEditTime: 2022-05-05 21:33:21
- * @LastEditors: Evan Zuo v_wangxiangbo01@baidu.com
+ * @LastEditTime: 2022-04-27 19:12:01
+ * @LastEditors: Please set LastEditors
  * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  * @FilePath: /ms-math/src/app.service.ts
  */
@@ -14,7 +14,7 @@ export class AppService {
     await new Promise((resolve) => {
       setTimeout(() => {
         resolve(1);
-      }, 2000);
+      }, 10000);
     });
     const words = str.trim().split(/\s+/);
     return words.reduce((a, c) => ((a[c] = (a[c] || 0) + 1), a), {});
